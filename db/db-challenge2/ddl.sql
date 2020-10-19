724d7061c62a8d60f183e4e72c711829779920ed
shihoono <shiho1449@gmail.com>
2020-10-19 20:42:53 +0900

    userテーブル　DLL文作成

---
+++ db/db-challenge2/ddl.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id                  INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    email               VARCHAR(100) NOT NULL,
+    password            VARCHAR(100) NOT NULL,
+    name                VARCHAR(100) NOT NULL,
+    indroduction        VARCHAR(1000), 
+    office_phone_number VARCHAR(13),
+    phone_number        VARCHAR(13),
+    is_deleted          TINYINT(1) NOT NULL DEFAULT 0,
+    created_at          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
