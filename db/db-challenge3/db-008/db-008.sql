0ff194b7ca0ee023745ff95b29d5619ffa722519
shihoono <shiho1449@gmail.com>
2020-10-21 10:12:39 +0900

    db-008 SQL文

---
+++ db/db-challenge3/db-008/db-008.sql
@@ -0,0 +1,3 @@
+SELECT u.name, r.name, DATE_FORMAT(ru.joined_at, '%Y-%m-%d') 
+FROM rooms_users ru LEFT JOIN users u on ru.user_id=u.id LEFT JOIN rooms r on ru.room_id=r.id 
+WHERE u.is_deleted='0' AND r.is_deleted='0' ORDER BY joined_at;
