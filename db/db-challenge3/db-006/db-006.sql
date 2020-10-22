689c013d021e80d5d0245aecc100eac4c3749599
shihoono <shiho1449@gmail.com>
2020-10-22 10:21:00 +0900

    改行追加

--- db/db-challenge3/db-006/db-006.sql
+++ db/db-challenge3/db-006/db-006.sql
@@ -1,1 +1,2 @@
-SELECT id, email, password, name, introduction, office_phone_number, phone_number, is_deleted, created_at, updated_at FROM users ORDER BY id;
+SELECT id, email, password, name, introduction, office_phone_number, phone_number, is_deleted, created_at, updated_at 
+FROM users ORDER BY id;
